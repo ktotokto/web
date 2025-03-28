e87d5105cc912c1633e389cf266c811912b27b06
YANDEX2 <nazarsilovskij79@gmail.com>
2025-03-28 20:04:12 +0300

    Список профессий

--- main.py
+++ main.py
@@ -17,12 +17,21 @@ def training(prof):
 @app.route('/list_prof/<string:list_input>')
 def list_prof(list_input):
     list_pr = ["инженер-иследователь", "пилот", "строитель", "экзобиолог", "врач",
-              "инженер по терраформированию", "климатолог",
-              "специалист по радиационной защите", "астрогеолог", "гляциолог",
-              "инженер жизнеобеспечения", "метеоролог", "оператор марсохода",
-              "киберинженер", "штурман", "пилот дронов"]
+               "инженер по терраформированию", "климатолог",
+               "специалист по радиационной защите", "астрогеолог", "гляциолог",
+               "инженер жизнеобеспечения", "метеоролог", "оператор марсохода",
+               "киберинженер", "штурман", "пилот дронов"]
     return render_template('list_prof.html',
                            list=list_input, list_prof=list_pr)
+
+
+@app.route('/auto_answer')
+@app.route('/answer')
+def auto_answer():
+    answer_dict = {"title": "Анкета", "surname": "Watny", "name": "Mark",
+                   "education": "выше среднего", "profession": "штурман марсохода", "sex": "male",
+                   "motivation": "Всегда мечтал застрять на Марсе!", "ready": True}
+    return render_template('auto_answer.html', dict=answer_dict, title=answer_dict["title"])
 
 
 if __name__ == '__main__':
